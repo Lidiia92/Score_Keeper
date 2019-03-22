a7b3fb7b767d1b62f65b3f9995c552ac2b8156cd
Lidii <lidiiany92@gmail.com>
2019-03-22 13:40:58 -0400

    Sorting players by their score

--- score-keeper/client/main.js
+++ score-keeper/client/main.js
@@ -8,7 +8,7 @@ import App from '../imports/ui/App';
 
 Meteor.startup( () => {
   Tracker.autorun(() => {
-    const players = Players.find().fetch();
+    const players = Players.find({}, {sort: {score: -1}}).fetch();
     const title = "Score Keeper";
     ReactDOM.render(<App title={title} players={players}/>, document.getElementById('app'));
   });
