6105fbe6f455d054bc0ad08ba0c79a881323cb8d
Lidii <lidiiany92@gmail.com>
2019-03-28 18:13:37 -0400

    Did more styling

--- score-keeper/imports/ui/App.js
+++ score-keeper/imports/ui/App.js
@@ -5,6 +5,12 @@ import TitleBar from './TitleBar';
 import AddPlayer from './AddPlayer';
 import PlayerList from './PlayerList';
 
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faMedal} from '@fortawesome/free-solid-svg-icons'
+
+library.add(faMedal)
+
 class App extends React.Component {
     render() {
         return (
--- score-keeper/imports/ui/Player.js
+++ score-keeper/imports/ui/Player.js
@@ -1,25 +1,39 @@
 import React from 'react';
 import {Players} from '../api/players';
 import PropTypes from 'prop-types';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+
+
 
 class Player extends React.Component {
 
+
   render() {
-    const player = this.props.player;
-    console.log(player);
+
+	const player = this.props.player;
+	let itemClassName = `item item--position-${player.rank}`;
+	let buttonRanked = `button--position-${player.rank}` 
+	let buttonsClasses = `button button--round`;
+
+	if(player.rank === 1 || player.rank === 2 || player.rank === 3) {
+		buttonsClasses = `button button--round ${buttonRanked}`
+	} else {
+		buttonsClasses = `button button--round`
+	}
+	
+	
     return (
-      <div key={player._id} className="item">
+      <div key={player._id} className={itemClassName}>
         <div className="player">
 			<div>
 				<h3 className="player__name">{player.name}</h3>
 				<p className="player__stats">{player.score} point(s).</p>
-				Rank: {player.rank}
-				Position: {player.position}
+				<h3>{player.rank === 1 || player.rank === 2 || player.rank === 3 ? <FontAwesomeIcon icon="medal" /> : ''} {player.position} place.</h3>
 			</div>
 			<div className="player__actions">
-				<button className="button button--round" onClick={() => Players.update({_id: player._id}, {$inc: {score: 1}})}>+1</button>
-				<button className="button button--round" onClick={() => Players.update({_id: player._id}, {$inc: {score: -1}})}>-1</button>
-				<button className="button button--round" onClick={() => Players.remove({_id: player._id})}>X</button>
+				<button className={buttonsClasses} onClick={() => Players.update({_id: player._id}, {$inc: {score: 1}})}>+1</button>
+				<button className={buttonsClasses} onClick={() => Players.update({_id: player._id}, {$inc: {score: -1}})}>-1</button>
+				<button className={buttonsClasses} onClick={() => Players.remove({_id: player._id})}>X</button>
 			</div>
         </div>
       </div>
